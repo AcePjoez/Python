4217ba59b24f9f2c74c472e00e58b2cc59d3ddc1
AcePjoez <37635775+AcePjoez@users.noreply.github.com>
2018-03-24 19:16:58 +0100

    Update Reference_3_HTML.py

--- Data analysis/Reference_3_HTML.py
+++ Data analysis/Reference_3_HTML.py
@@ -1,12 +1,12 @@
 import quandl
 import pandas as pd
 
-api_key = '328PZtbFt7_s17pLYwP2'
+api_key = '328PZtbFt7_s17pLYwP2'    
 
-df = quandl.get('FMAC/HPI_AK', authtoken=api_key)
+df = quandl.get('FMAC/HPI_AK', authtoken=api_key)                                       # Accessing quandl database
 print(df.head())
 
-fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')
+fiddy_states = pd.read_html('https://simple.wikipedia.org/wiki/List_of_U.S._states')    # Accessing html database
 
 # This is a list
 print(fiddy_states)
@@ -17,5 +17,6 @@ print(fiddy_states[0])
 # This is a column:
 print(fiddy_states[0][1])
 
+# Printing all hpi codes
 for abbv in fiddy_states[0][1][1:]:
     print('FMAC/HPI_' + str(abbv))
